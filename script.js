3d9807aebf04e3552d8032b8f3239cb7535031f0
Anna Taules <anna.taules@gmail.com>
2024-10-20 19:46:39 +0200

    project readey again

--- script.js
+++ script.js
@@ -7,22 +7,30 @@ function plusSlides(n) {
 }
 
 // Thumbnail image controls
-function currentSlide(n) {
+/* function currentSlide(n) {
   showSlides(slideIndex = n);
-}
+} */
 
+let slides = document.getElementsByClassName("mySlides");
 function showSlides(n) {
   let i;
   let slides = document.getElementsByClassName("mySlides");
-  let dots = document.getElementsByClassName("dot");
+//   let dots = document.getElementsByClassName("dot");
   if (n > slides.length) {slideIndex = 1}
   if (n < 1) {slideIndex = slides.length}
   for (i = 0; i < slides.length; i++) {
     slides[i].style.display = "none";
   }
-  for (i = 0; i < dots.length; i++) {
+  /* for (i = 0; i < dots.length; i++) {
     dots[i].className = dots[i].className.replace(" active", "");
-  }
+  }  */
   slides[slideIndex-1].style.display = "block";
-  dots[slideIndex-1].className += " active";
-} 
+  //dots[slideIndex-1].className += " active";
+}  
+
+
+
+
+
+
+
